81aa5109324e23cf331b121a506288db4f5d6cef
Baipyrus <simon.waltemathe@gmail.com>
2026-02-11 15:45:11 +0100

    fix: magic number on exit code for font download

--- scripts/download-font.ts
+++ scripts/download-font.ts
@@ -5,6 +5,7 @@ import { join } from 'path';
 const NERDFONTS_RELEASE = 'https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest';
 const DOWNLOAD_LOCATION = 'static/fonts/';
 const ARCHIVE_NAME = 'CascadiaCode.tar.xz';
+const EXIT_SUCCESS = 0;
 const FONT_NAME = 'CaskaydiaCoveNerdFontMono-Regular.ttf';
 
 type MockGitHubAPI = {
@@ -18,7 +19,7 @@ async function downloadFont() {
 	const TARGET_PATH = join(DOWNLOAD_LOCATION, ARCHIVE_NAME);
 	if (existsSync(TARGET_PATH)) {
 		console.info('Font archive already downloaded, exiting...');
-		process.exit(0);
+		process.exit(EXIT_SUCCESS);
 	}
 	console.info('Fetching latest Nerd Font release...');
 
