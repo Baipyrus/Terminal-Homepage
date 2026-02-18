81c9417d4502e4cab8781003d9290d9c7f541935
Baipyrus <simon.waltemathe@gmail.com>
2026-02-18 22:01:53 +0100

    feat: stylized error message on navigation error

---
+++ src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import { CATPPUCCIN_MOCHA } from '$lib/config/terminal';
+
+	let { data } = $props();
+</script>
+
+<div
+	class="flex h-screen w-screen items-center justify-center"
+	style:background-color={CATPPUCCIN_MOCHA.background}
+>
+	<p class="text-lg font-bold" style:color={CATPPUCCIN_MOCHA.red}>
+		[{page.status}] {data.httpMethod}
+		{page.url.pathname}
+	</p>
+</div>
