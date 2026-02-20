82a943f6e490e21859e4b005aa797bc78253565c
Baipyrus <simon.waltemathe@gmail.com>
2026-02-20 01:07:41 +0100

    chore: fixed example command and added more customization details

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -47,23 +47,33 @@
 
 		// Initialize custom shell for xterm.js
 		const shell = new Shell(terminal);
-		shell.Initialize([
-			// {
-			// 	name: 'example',
-			// 	description: 'Example of a custom command',
-			// 	action: ({ terminal, args }) => {
-			// 		terminal.writeln(
-			// 			"Any custom command can easily access this shell's corresponding terminal."
-			// 		);
-			// 		terminal.writeln(
-			// 			'They can also accept any number of positional arguments, separated by spaces.'
-			// 		);
-			// 		terminal.writeln(
-			// 			`Custom command's arguments are: [${args.map((str) => `'${str}'`).join(', ')}]`
-			// 		);
+		shell.Initialize({
+			// // To add a custom prompt for the shell, you can set something like the following.
+			// // It will set a white text starting with the username that is currently logged in
+			// // ('root' while logged out), a fixed hostname you can define once, and the current
+			// // "path" (the chat you are in). All static strings and the formatting as a whole
+			// // are all completely up to your customization!
+			// prompt: "$user@hostname $pwd$ ",
+			//
+			// // Also, add custom commands like so:
+			// commands: [
+			// 	{
+			// 		name: 'example',
+			// 		description: 'Example of a custom command',
+			// 		action: ({ terminal, args }) => {
+			// 			terminal.writeln(
+			// 				"Any custom command can easily access this shell's corresponding terminal."
+			// 			);
+			// 			terminal.writeln(
+			// 				'They can also accept any number of positional arguments, separated by spaces.'
+			// 			);
+			// 			terminal.writeln(
+			// 				`Custom command's arguments are: [${args.map((str) => `'${str}'`).join(', ')}]`
+			// 			);
+			// 		}
 			// 	}
-			// }
-		]);
+			// ]
+		});
 	}
 </script>
 
