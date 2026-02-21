9b52ecdc97f151c0a95f2d2be8b62fd37e9448df
Baipyrus <simon.waltemathe@gmail.com>
2026-02-21 21:40:29 +0100

    chore: remove unused db table remnant from demo stage

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,14 +1,4 @@
-import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
-
-const DEFAULT_TASK_PRIORITY = 1;
-
-export const task = sqliteTable('task', {
-	id: text('id')
-		.primaryKey()
-		.$defaultFn(() => crypto.randomUUID()),
-	title: text('title').notNull(),
-	priority: integer('priority').notNull().default(DEFAULT_TASK_PRIORITY)
-});
+import { sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const directory = sqliteTable('directory', {
 	path: text('path').primaryKey()
