69dbc0babc23af08cc87a5a8054b580eac295840
Baipyrus <simon.waltemathe@gmail.com>
2026-03-01 18:56:31 +0100

    feat: sveltekit error handling with HandleServerError hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,10 @@
-import type { Handle } from '@sveltejs/kit';
+import type { Handle, HandleServerError } from '@sveltejs/kit';
+import { sequence } from '@sveltejs/kit/hooks';
 import { building } from '$app/environment';
 import { auth } from '$lib/server/auth';
 import { svelteKitHandler } from 'better-auth/svelte-kit';
+import logger from '$lib/server/logger';
+import { BAD_REQUEST } from '$lib/constants/http';
 
 const handleBetterAuth: Handle = async ({ event, resolve }) => {
 	const session = await auth.api.getSession({ headers: event.request.headers });
@@ -15,4 +18,26 @@ const handleBetterAuth: Handle = async ({ event, resolve }) => {
 	return svelteKitHandler({ event, resolve, auth, building });
 };
 
-export const handle: Handle = handleBetterAuth;
+const handleLogging: Handle = async ({ event, resolve }) => {
+	const response = await resolve(event);
+	const { method } = event.request;
+	const { pathname } = event.url;
+	const { status } = response;
+
+	if (status >= BAD_REQUEST) logger.error(`${status} ${method} ${pathname}`, { label: 'HTTP' });
+	else logger.info(`${status} ${method} ${pathname}`, { label: 'HTTP' });
+
+	return response;
+};
+
+export const handle: Handle = sequence(handleBetterAuth, handleLogging);
+
+export const handleError: HandleServerError = ({ error, event }) => {
+	const { method } = event.request;
+	const { pathname } = event.url;
+
+	const errorMessage = error instanceof Error ? error.message : JSON.stringify(error);
+	logger.error(`${method} ${pathname} - ${errorMessage}`, { label: 'INTERNAL' });
+
+	if (error instanceof Error && error.stack) logger.debug(error.stack, { label: 'STACK' });
+};
