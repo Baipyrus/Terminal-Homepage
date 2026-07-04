025eec1e8bb247bd3ff64e794f12d728e3eba699
Baipyrus <simon.waltemathe@gmail.com>
2026-07-04 16:02:43 +0200

    fix: svelte/valid-prop-names-in-kit-pages lint error
    Error message: error  disallow invalid props in SvelteKit route components
    Fixed by: Accessing the data via the preexisting page state import

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/state';
 	import { CATPPUCCIN_MOCHA } from '$lib/config/terminal';
-	import type { PageProps } from './$types';
-
-	const { data }: PageProps = $props();
 </script>
 
 <div
@@ -11,7 +8,7 @@
 	style:background-color={CATPPUCCIN_MOCHA.background}
 >
 	<p class="text-lg font-bold" style:color={CATPPUCCIN_MOCHA.red}>
-		[{page.status}] {data?.httpMethod ?? ''}
+		[{page.status}] {page.data.httpMethod ?? ''}
 		{page.url.pathname}
 	</p>
 </div>
